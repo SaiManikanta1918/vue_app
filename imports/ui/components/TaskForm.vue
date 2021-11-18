c9bb61798b2abbdf98e62f5f3af019145b26e259
SaiManikanta1918 <81068136+SaiManikanta1918@users.noreply.github.com>
2021-11-19 01:04:31 +0530

    Add files via upload

---
+++ imports/ui/components/TaskForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form class="task-form" @submit.prevent="handleSubmit">
+    <input
+        type="text"
+        placeholder="Type to add new tasks"
+        v-model="newTask"
+    />
+    <button type="submit">Add Task</button>
+  </form>
+</template>
+
+<script>
+import Vue from "vue";
+import { TasksCollection } from "../../api/collections/TasksCollection";
+export default {
+  data() {
+    return {
+      newTask: ""
+    }
+  },
+  methods: {
+    handleSubmit(event) {
+      if (this.newTask.length === 0) return;
+      
+      Meteor.call('tasks.insert', this.newTask.trim())
+      // Clear form
+      this.newTask = "";
+    }
+  },
+}
+</script>
